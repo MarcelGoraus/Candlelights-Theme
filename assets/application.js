7e5c6d2616ee001a1fbc131afc2306d2d801fc75
Marcel Goraus <128155279+MarcelGoraus@users.noreply.github.com>
2024-01-02 23:21:33 +0000

    v 0.1

--- assets/application.js
+++ assets/application.js
@@ -46,22 +46,6 @@ var formatMoney = function (cents, format) {
   return formatString.replace(placeholderRegex, value);
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.querySelector(".navigation-toggle");
-  const sidebarMenu = document.querySelector(".sidebar-menu");
-  const sidebarOverlay = document.querySelector(".sidebar-overlay");
-
-  toggleButton.addEventListener("click", function () {
-    sidebarMenu.classList.toggle("active"); // Aktiviert/deaktiviert die Klasse "active" der Sidebar
-    sidebarOverlay.classList.toggle("active"); // Aktiviert/deaktiviert die Klasse "active" des Overlays
-  });
-
-  sidebarOverlay.addEventListener("click", function () {
-    sidebarMenu.classList.remove("active"); // Deaktiviert die Klasse "active" der Sidebar beim Klicken auf das Overlay
-    sidebarOverlay.classList.remove("active"); // Deaktiviert die Klasse "active" des Overlays beim Klicken darauf
-  });
-});
-
 AOS.init({
   once: true,
 });
